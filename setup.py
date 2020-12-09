21351ed07f1a50ed3440e63d0239808a3bcc757a
thegismar <thegismar@gmail.com>
2020-12-08 22:50:02 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(
-    name="w3u-THEGISMAR", # Replace with your own username
+    name="w3utils",
     version="0.0.1",
     author="thegismar",
     description="Some convenience functions to make web3 coding easier.",
