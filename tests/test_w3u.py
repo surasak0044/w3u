0d68fee3974d17e27c9fbdc1c9e782295c63ca69
thegismar <thegismar@gmail.com>
2020-12-08 21:44:48 -0300

    Initial Commit

---
+++ tests/test_w3u.py
@@ -0,0 +1,24 @@
+import w3u
+
+
+def test_erc20_address():
+    assert w3u.erc20("DAI")["address"] == "0x6b175474e89094c44da98b954eedeac495271d0f"
+
+
+def test_erc20_symbol():
+    assert w3u.erc20("0x6b175474e89094c44da98b954eedeac495271d0f")["symbol"] == "DAI"
+
+
+def test_erc20_dec():
+    assert w3u.erc20("DAI")["decimals"] == 18
+
+
+def test_get_transactions_by_account():
+    assert (
+        w3u.get_transactions_by_account("0x7fBaF24Be5Fb8eaefA5aDd9AF3F7052d3fF52E40")
+        != 1
+    )
+
+
+def test_get_abi():
+    assert w3u.get_abi("0x6b175474e89094c44da98b954eedeac495271d0f") != 1
---
+++ w3u/w3u.py
@@ -0,0 +1,102 @@
+import pandas as pd
+import os
+import requests
+import time
+
+URL = "https://api.etherscan.io/api?"
+KEY = os.getenv("ETHERSCAN_TOKEN")
+
+
+def erc20(data):
+    """
+    :param data: either symbol (dont'care for upper/lowercase, or address (checks for starting with 0x and decides)
+    :return: list of [symbol or address, decimals]
+    """
+    url = "https://tokens.coingecko.com/uniswap/all.json"
+    r = None
+
+    while True:
+        try:
+            r = requests.get(url).json()
+        except requests.exceptions.Timeout:
+            time.sleep(5)
+            continue
+        except requests.exceptions.TooManyRedirects as e:
+            print(f"URL cannot be reached. {e}")
+            break
+        except requests.exceptions.RequestException as e:
+            raise SystemExit(e)
+        else:
+            break
+
+    r = pd.DataFrame(r["tokens"])
+
+    if data.startswith("0x"):
+        ret = r.loc[r["address"] == data, ["symbol", "decimals"]]
+        ret.reset_index(drop=True, inplace=True)
+        return ret.loc[0]
+
+    else:
+        ret = r.loc[r["symbol"] == data, ["address", "decimals"]]
+        ret.reset_index(drop=True, inplace=True)
+        return ret.loc[0]
+
+
+def get_abi(address):
+    """
+    :param address: str of abi ethereum address
+    :return: string of abi
+    """
+    r = None
+    url = URL + f"module=contract&action=getabi&address={address}&apiKey={KEY}"
+
+    while True:
+        try:
+            r = requests.get(url).json()
+        except requests.exceptions.Timeout:
+            time.sleep(5)
+            continue
+        except requests.exceptions.TooManyRedirects as e:
+            print(f"URL cannot be reached. {e}")
+            break
+        except requests.exceptions.RequestException as e:
+            raise SystemExit(e)
+        else:
+            break
+
+    if r["status"] != "1":
+        return 1
+    else:
+        return r["result"]
+
+
+def get_transactions_by_account(account):
+    """
+    queries etherscan and gets all the transactions that involve a certain acount
+    :param account:
+    :return:
+    """
+    r = None
+
+    url = (
+        URL + f"module=account&action=txlist&address={account}&startblock=0&"
+        f"endblock=99999999&sort=asc&apikey={KEY}"
+    )
+    while True:
+        try:
+            r = requests.get(url).json()
+        except requests.exceptions.Timeout:
+            time.sleep(5)
+            continue
+        except requests.exceptions.TooManyRedirects as e:
+            print(f"URL cannot be reached. {e}")
+            break
+        except requests.exceptions.RequestException as e:
+            raise SystemExit(e)
+        else:
+            break
+
+    if r["status"] != "1":
+        return 1
+    else:
+        return r["result"]
